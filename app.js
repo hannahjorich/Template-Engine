b2d9fd7ba6444d5e91782f755a8b9f28fbf55e64
hannahjorich <hannah.jorich@gmail.com>
2020-08-28 15:55:22 -0600

    added regex patterns

--- app.js
+++ app.js
@@ -20,33 +20,35 @@ function askManager() {
         message: "What is your manager name?",
         name: "managerName",
         validate: (answer) => {
-          if (answer !== "") {
-            return true;
-          }
-          return "No blank fields";
-        },
+            const pass = answer.match(/^[a-zA-Z]+$/)
+            if(pass) {
+                return true;
+            }
+            return "Please enter a valid name"
+              },
       },
       {
         type: "input",
         message: "What is your email?",
         name: "managerEmail",
         validate: (answer) => {
-          if (answer !== "") {
-            return true;
-          }
-          return "No blank fields";
-        },
+            const pass = answer.match(/\S+@\S+\.\S+/)
+            if(pass) {
+                return true;
+            }
+            return "Please enter a valid email"
+              },
       },
       {
         type: "input",
         message: "What is your manager id?",
         name: "managerId",
         validate: (answer) => {
-          if (answer !== "") {
-            return true;
-          }
-          return "No blank fields";
-        },
+            if (answer !== "") {
+                return true;
+              }
+              return "No blank fields";
+            },
       },
       {
         type: "input",
@@ -123,22 +125,24 @@ function askEngineer() {
           message: "What is your engineer's name?",
           name: "engineerName",
           validate: (answer) => {
-            if (answer !== "") {
-              return true;
+            const pass = answer.match(/^[a-zA-Z]+$/)
+            if(pass) {
+                return true;
             }
-            return "No blank fields";
-          },
+            return "Please enter a valid name"
+              },
         },
         {
           type: "input",
           message: "What is your email?",
           name: "engineerEmail",
           validate: (answer) => {
-            if (answer !== "") {
-              return true;
+            const pass = answer.match(/\S+@\S+\.\S+/)
+            if(pass) {
+                return true;
             }
-            return "No blank fields";
-          },
+            return "Please enter a valid email"
+              },
         },
         {
           type: "input",
@@ -146,10 +150,10 @@ function askEngineer() {
           name: "engineerId",
           validate: (answer) => {
             if (answer !== "") {
-              return true;
-            }
-            return "No blank fields";
-          },
+                return true;
+              }
+              return "No blank fields";
+            },
         },
         {
           type: "input",
@@ -183,11 +187,12 @@ function askEngineer() {
           message: "What is your intern's name?",
           name: "internName",
           validate: (answer) => {
-            if (answer !== "") {
-              return true;
+            const pass = answer.match(/^[a-zA-Z]+$/)
+            if(pass) {
+                return true;
             }
-            return "No blank fields";
-          },
+            return "Please enter a valid name"
+              },
         },
         {
           type: "input",
@@ -207,10 +212,10 @@ function askEngineer() {
           name: "internId",
           validate: (answer) => {
             if (answer !== "") {
-              return true;
-            }
-            return "No blank fields";
-          },
+                return true;
+              }
+              return "No blank fields";
+            },
         },
         {
           type: "input",
@@ -244,28 +249,3 @@ function askEngineer() {
   }
   
 askManager();
-
-
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ``
