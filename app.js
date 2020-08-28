e6240444619d058b3676ff6a21ada5091e99b1f9
hannahjorich <hannah.jorich@gmail.com>
2020-08-28 15:34:16 -0600

    added questions

--- app.js
+++ app.js
@@ -8,7 +8,243 @@ const fs = require("fs");
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
+const team = [];
+
 const render = require("./lib/htmlRenderer");
+
+function askManager() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is your manager name?",
+        name: "managerName",
+        validate: (answer) => {
+          if (answer !== "") {
+            return true;
+          }
+          return "No blank fields";
+        },
+      },
+      {
+        type: "input",
+        message: "What is your email?",
+        name: "managerEmail",
+        validate: (answer) => {
+          if (answer !== "") {
+            return true;
+          }
+          return "No blank fields";
+        },
+      },
+      {
+        type: "input",
+        message: "What is your manager id?",
+        name: "managerId",
+        validate: (answer) => {
+          if (answer !== "") {
+            return true;
+          }
+          return "No blank fields";
+        },
+      },
+      {
+        type: "input",
+        message: "What is your office number",
+        name: "managerNumber",
+        validate: (answer) => {
+          if (answer !== "") {
+            return true;
+          }
+          return "No blank fields";
+        },
+      },
+    ])
+    .then(function (answer) {
+      const manager = new Manager(
+        answer.managerName,
+        answer.managerId,
+        answer.managerEmail,
+        answer.managerNumber
+      );
+      team.push(manager);
+      createEmployeePrompt()
+    });
+}
+function createEmployeePrompt() {
+  inquirer.prompt([
+    {
+      type: "list",
+      name: "employees",
+      message: "Would you like to add any more employees?",
+      choices: [
+        {
+          name: "Manager",
+          value: "Manager",
+        },
+        {
+          name: "Engineer",
+          value: "Engineer",
+        },
+        {
+          name: "Intern",
+          value: "Intern",
+        },
+        {
+          name: "None",
+          value: "None",
+        },
+      ],
+    },
+  ]).then(function(answers){
+      switch(answers.employees) {
+        case "Manager":
+              askManager();
+        break;
+        case "Engineer":
+            askEngineer();
+        break;
+        case "Intern":
+            askIntern();
+        break;
+        
+        default: 
+            console.log("building team");
+            buildTeam();
+  }
+  })
+}
+
+function askEngineer() {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          message: "What is your engineer's name?",
+          name: "engineerName",
+          validate: (answer) => {
+            if (answer !== "") {
+              return true;
+            }
+            return "No blank fields";
+          },
+        },
+        {
+          type: "input",
+          message: "What is your email?",
+          name: "engineerEmail",
+          validate: (answer) => {
+            if (answer !== "") {
+              return true;
+            }
+            return "No blank fields";
+          },
+        },
+        {
+          type: "input",
+          message: "What is your engineer's id?",
+          name: "engineerId",
+          validate: (answer) => {
+            if (answer !== "") {
+              return true;
+            }
+            return "No blank fields";
+          },
+        },
+        {
+          type: "input",
+          message: "What is your Github",
+          name: "engineerGit",
+          validate: (answer) => {
+            if (answer !== "") {
+              return true;
+            }
+            return "No blank fields";
+          },
+        },
+      ])
+      .then(function (answer) {
+        const engineer = new Engineer(
+          answer.engineerName,
+          answer.engineerId,
+          answer.engineerEmail,
+          answer.engineerGit
+        );
+        team.push(engineer);
+        createEmployeePrompt()
+      });
+  }
+
+  function askIntern() {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          message: "What is your intern's name?",
+          name: "internName",
+          validate: (answer) => {
+            if (answer !== "") {
+              return true;
+            }
+            return "No blank fields";
+          },
+        },
+        {
+          type: "input",
+          message: "What is your email?",
+          name: "internEmail",
+          validate: (answer) => {
+        const pass = answer.match(/\S+@\S+\.\S+/)
+        if(pass) {
+            return true;
+        }
+        return "Please enter a valid email"
+          },
+        },
+        {
+          type: "input",
+          message: "What is your intern's id?",
+          name: "internId",
+          validate: (answer) => {
+            if (answer !== "") {
+              return true;
+            }
+            return "No blank fields";
+          },
+        },
+        {
+          type: "input",
+          message: "What school did you attend?",
+          name: "internSchool",
+          validate: (answer) => {
+            if (answer !== "") {
+              return true;
+            }
+            return "No blank fields";
+          },
+        },
+      ])
+      .then(function (answer) {
+        const intern = new Intern (
+          answer.internName,
+          answer.internId,
+          answer.internEmail,
+          answer.internSchool
+        );
+        team.push(intern);
+        createEmployeePrompt()
+      });
+  }
+
+  function buildTeam() {
+      if(!fs.existsSync(OUTPUT_DIR)){
+          fs.mkdirSync(OUTPUT_DIR)
+      }
+      fs.writeFileSync(outputPath, render(team), "utf-8")
+  }
+  
+askManager();
+
 
 
 // Write code to use inquirer to gather information about the development team members,
@@ -32,4 +268,4 @@ const render = require("./lib/htmlRenderer");
 // and Intern classes should all extend from a class named Employee; see the directions
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+// for the provided `render` function to work! ``
